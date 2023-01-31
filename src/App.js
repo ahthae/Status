af78913b780c983e1546ebc21ae279ff73721e20
ahthae <ahthaeon@pm.me>
2023-01-31 01:42:59 -0800

    Clear boilerplate page, add Incidents component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+function Incidents() {
+  return (
+    <Container className="Incidents rounded">
+      <Row>
+        <Col><h3 className="Incidents-Header m-2">Latest Incident</h3></Col>
+      </Row>
+      <Row>
+        <Col><h4 className="Incidents-Title m-2 text-center">Expected maintenance schedule for 2023-01-31 04:30:00 GMT-7</h4></Col>
+        <Col xs={8}><p className="Incidents-Body m-2">
+            The following services will be down on January 31st between 4:30 AM PST and 6:30 AM PST while we perform regular maintenance:<br/>
+            - Authentication<br/>
+            - API<br/>
+            - Git<br/>
+        </p></Col>
+      </Row>
+      <Row>
+        <svg className="mb-2" width="2em" height="2em" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <rect width="48" height="48" fill="white" fill-opacity="0.01"/>
+        <path d="M40 28L24 40L8 28" stroke="#747c92" stroke-width="4" stroke-linecap="round" stroke-linejoin="round"/>
+        <path d="M8 10H40" stroke="#747c92" stroke-width="4" stroke-linecap="round"/>
+        <path d="M8 18H40" stroke="#747c92" stroke-width="4" stroke-linecap="round"/>
+        </svg>
+      </Row>
+    </Container>
+  );
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Incidents/>
     </div>
   );
 }

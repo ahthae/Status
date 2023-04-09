9ca0b3727d268f6be802ea71547fe07b1303dd51
ahthae <ahthaeon@pm.me>
2023-04-09 16:23:38 -0700

    Add more info properties to Response model

--- Status.Server/Status.Core/Models/Response.cs
+++ Status.Server/Status.Core/Models/Response.cs
@@ -8,5 +8,7 @@ namespace Status.Core.Models
         public TimeSpan ResponseTime { get; set; }
         [Required]
         public DateTime Timestamp { get; set; }
+        public bool Success { get; set; }
+        public string? Information { get; set; }
     }
 }
